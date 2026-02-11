a00ac65fca524e79069a7031da62a061d20de0ac
Theogene <igiranezatheogene4@gmail.com>
2026-02-12 01:15:05 +0200

    Advanced Props Learning

--- src/App.jsx
+++ src/App.jsx
@@ -14,51 +14,48 @@ import { useState } from "react";
 //   )
 // }
 
-// function Header() {
-//   return(
-//     <div className="flex gap-1 justify-center p-4 bg-[#F55A5A] text-white rounded-t-md">
-//       <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-globe-icon lucide-globe"><circle cx="12" cy="12" r="10"/><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/><path d="M2 12h20"/></svg></p>
-//       <p className="text-sm">my travel journal.</p>
-//     </div>
-//   )
-// }
+function Header() {
+  return(
+    <div className="flex gap-1 justify-center p-4 bg-[#F55A5A] text-white rounded-t-md">
+      <p><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-globe-icon lucide-globe"><circle cx="12" cy="12" r="10"/><path d="M12 2a14.5 14.5 0 0 0 0 20 14.5 14.5 0 0 0 0-20"/><path d="M2 12h20"/></svg></p>
+      <p className="text-sm">my travel journal.</p>
+    </div>
+  )
+}
 
-// function App() {
-//   return (
-//     <Header />
-//   )
-// }
-// export default App
+function Entry(props){
 
-import Joke from "./Joke"
-
-let joke = [
-    {
-        setup: "I got my daughter a fridge for her birthday.",
-        punchline: "I can't wait to see her face light up when she opens it."
-    },
-    {
-        setup:"How did the hacker escape the police?",
-        punchline:"He just ransomware!"
-    },
-    {
-        setup:"Why don't pirates travel on mountain roads?",
-        punchline:"Scurvy."
-    },
-    {
-        setup:"Why do bees stay in the hive in the winter?",
-        punchline:"Swarm."
-    },
-    {
-        setup:"What's the best thing about Switzerland?",
-        punchline : "I don't know, but the flag is a big plus!"
-    }
-]
-
-export default function App() {
-    return (
-        <main>
-            {joke.map(v => <Joke setup = {v.setup} punchline = {v.punchline} />)}
-        </main>
+    return(
+        <div className="flex mx-10 gap-5 my-5">
+            <div className="overflow-hidden">
+                <img className="w-96 h-full rounded-[5px]" src={props.img.src} alt={props.img.alt} />
+            </div>
+            <div className="">
+                <p className="text-[13px] font-semibold">{props.country}<span className="ml-4 underline hover:cursor-pointer">View on Google Maps</span></p>
+                <p className="font-bold text-2xl">{props.place}</p>
+                <p className="font-bold my-3">{props.date}</p>
+                <p className="text-[13px] font-semibold">{props.discriptions}</p>
+            </div>
+        </div>
     )
 }
+
+function App() {
+  return (
+    <>
+        <Header />
+        <Entry 
+            img = {{
+                src: "https://scrimba.com/links/travel-journal-japan-image-url",
+                alt: "Mount Fuji"
+            }}
+            country = 'JAPAN'
+            place = 'Mount Fuji'
+            date = "12 Jan, 2021 - 24 Jan, 2021"
+            discriptions = "Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists."
+        />
+    </>
+    
+  )
+}
+export default App
--- src/Joke.jsx
+++
@@ -1,9 +0,0 @@
-export default function Joke(props) {
-    return (
-        <>
-            <p className="m-5 font-bold text-purple-500">Setup: {props.setup}</p>
-            <p className="m-5 mx-10 font-semibold">Punchline: {props.punchline}</p>
-            <hr />
-        </>
-    )
-}
