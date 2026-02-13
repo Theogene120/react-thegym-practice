6ee48c691a97838c204eecf12c374f976171a644
Theogene <igiranezatheogene4@gmail.com>
2026-02-13 17:50:40 +0200

    Some changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,46 @@
 
 
 
+function Header() {
+  return(
+    <div className="flex gap-1 justify-center items-center p-5 border rounded-t-md bg-white">
+      <img className="w-7" src="chef-claude-icon.png" alt="log" />
+      <p className="text-lg font-semibold">Chef Claude</p>
+    </div>
+  )
+}
 
+function Main() {
+    const ingredients = ["Chicken", "Oregano", "Tomatoes"]
+    
+    const list = ingredients.map(ingredient => (
+        <li key={ingredient}>{ingredient}</li>
+    ))
+
+    function handleSubmision(e) {
+        e.preventDefault()
+        console.log("Form submitted")
+    }
+    return (
+        <>
+            <form onSubmit={handleSubmision} className="my-12 flex justify-center gap-5">
+                <input className="border-2 border-gray-300 rounded-md w-80 outline-none pl-3" placeholder="e.g. oregano" type="text" name="text" id="1" />
+                <button  className="bg-[#141413] text-white px-6 rounded-md h-8 text-sm font-semibold text-opacity-90">+ Add ingredient</button>
+            </form>
+            <ul className="list-disc ml-20">{list}</ul>
+        </>
+    )
+}
 
 function App() {
-    return <p>Hello world</p>
+    
+    return (
+        <>
+            <Header />
+            <Main />
+        </>
+    
+)
 }
 
 export default App
