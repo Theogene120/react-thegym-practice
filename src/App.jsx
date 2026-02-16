4358199b4712e31dbb493155f7b666f0a8d5b765
Theogene <igiranezatheogene4@gmail.com>
2026-02-16 22:30:17 +0200

    Chef claude project finished

--- src/App.jsx
+++ src/App.jsx
@@ -17,17 +17,15 @@ function Main() {
         <li key={ingredient}>{ingredient}</li>
     ))
 
-    function handleSubmision(e) {
-        e.preventDefault()
+    function handleSubmision(formData) {
         console.log("Form submitted") // for checking
-        const formData = new FormData(e.currentTarget)
         const newIngredients = formData.get('ingredient') // We pass form name as string
 
         setIngredients(prevIngredients => [...prevIngredients, newIngredients])
     }
     return (
         <>
-            <form onSubmit={handleSubmision} className="my-12 flex justify-center gap-5">
+            <form action={handleSubmision} className="my-12 flex justify-center gap-5">
                 <input className="border-2 border-gray-300 rounded-md w-80 outline-none pl-3" placeholder="e.g. oregano" type="text" name="ingredient" id="1" />
                 <button  className="bg-[#141413] text-white px-6 rounded-md h-8 text-sm font-semibold text-opacity-90">+ Add ingredient</button>
             </form>
