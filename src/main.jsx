16e8bb13eae74de47894c2b309e62c0f024c8989
Theogene <igiranezatheogene4@gmail.com>
2026-02-16 13:48:19 +0200

    Form stuff

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,12 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
-import Profile from './components/UpdateObject.jsx'
+//import App from './App.jsx'
+import FormSkills from './FormSkills.jsx'
 import './index.css'
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App  />
+    <FormSkills  />
   </StrictMode>,
 )
