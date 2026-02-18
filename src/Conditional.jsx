4f472b7bdd6297c0aa91b0af3b2cbec57739c3f7
Theogene <igiranezatheogene4@gmail.com>
2026-02-18 21:56:35 +0200

    conditional rendering

---
+++ src/Conditional.jsx
@@ -0,0 +1,29 @@
+import React from "react"
+
+export default function Conditional() {
+    const [messages, setMessages] = React.useState(["a", "b"])
+    /**
+     * Challenge:
+     * - If there are no unread messages, display "You're all caught up!"
+     * - If there's exactly 1 unread message, it should read "You have 
+     *   1 unread message" (singular)
+     * - If there are > 1 unread messages, display "You have <n> unread
+     *   messages" (plural)
+     */
+    function checkArr(){
+        
+        if(messages.length == 0){
+            return <h1>You're all caught up!</h1>
+        }else if(messages.lenght > 1){
+            return <h1>You have {messages.length} messages</h1>
+        }else{
+            return <h1>You have 1 unread message</h1>
+        }
+        
+    }
+    return (
+        <div>
+            checkArr()
+        </div>
+    )
+}
