64cafd6c4bc4ef8a8da5b7c0fb4102f4f0ce02cf
Theogene <igiranezatheogene4@gmail.com>
2026-02-23 22:36:13 +0200

    change

--- src/App.jsx
+++ src/App.jsx
@@ -57,7 +57,7 @@ function Main() {
                 <li>3 cloves garlic, minced</li>
                 <li>2 tablespoons tomato paste</li>
                 <li>1 (28 oz) can crushed tomatoes</li>
-                <li>1 cup beef broth</li>
+                {/* <li>1 cup beef broth</li> */}
                 <li>1 teaspoon dried oregano</li>
                 <li>1 teaspoon dried basil</li>
                 <li>Salt and pepper to taste</li>
