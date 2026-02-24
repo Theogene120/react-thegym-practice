0ca34fd537fdea24edc441e15b2390d00298f09f
Theogene <igiranezatheogene4@gmail.com>
2026-02-24 15:38:17 +0200

    render component with map

--- src/components/ButtonSeries.jsx
+++ src/components/ButtonSeries.jsx
@@ -1,15 +1,19 @@
 import React from  'react'
 import pads from "./pads"
 
+function Pad(props){
+    return (
+    <button 
+        style={{backgroundColor: props.color}}
+        className='py-7 px-8 my-5 mx-8 '>
+    </button>
+)
+}
+
 export default function ButtonSeries(props) {
    
     let [button, setButton] = React.useState(pads)
-    let btn = button.map(b => <button 
-        key={b.id}
-        style={{backgroundColor: props.darkMode ? '#222222' : '#cccccc'}}
-        className='py-5 px-8 border-2 border-sky-500 my-5 mx-8 bg-sky-200'>
-            {b.id}
-        </button>)
+    let btn = button.map(b => <Pad key={b.id} color={b.color}/> )
     return (
         <main>
             <div className="pad-container">
