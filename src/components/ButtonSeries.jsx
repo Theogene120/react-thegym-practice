f25ea5313a03a9a4c24cb4a1277e1da68fff3540
Theogene <igiranezatheogene4@gmail.com>
2026-02-25 12:47:02 +0200

    some changes

--- src/components/ButtonSeries.jsx
+++ src/components/ButtonSeries.jsx
@@ -5,15 +5,18 @@ function Pad(props){
     return (
     <button 
         style={{backgroundColor: props.color}}
-        className='py-7 px-8 my-5 mx-8 '>
+        className={`py-9 px-10 my-5 mx-8  rounded-md ${props.on ? 'opacity-100': 'opacity-30'}`}>
     </button>
 )
 }
 
 export default function ButtonSeries(props) {
+
+    
    
     let [button, setButton] = React.useState(pads)
-    let btn = button.map(b => <Pad key={b.id} color={b.color}/> )
+    
+    let btn = button.map(b => <Pad key={b.id} color={b.color} on={b.on}/> )
     return (
         <main>
             <div className="pad-container">
