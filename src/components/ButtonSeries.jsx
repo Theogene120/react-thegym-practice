7d50065de455c0e40b7350fcbd49697b7a557a7c
Theogene <igiranezatheogene4@gmail.com>
2026-02-26 23:44:44 +0200

    changes

--- src/components/ButtonSeries.jsx
+++ src/components/ButtonSeries.jsx
@@ -2,8 +2,17 @@ import React from  'react'
 import pads from "./pads"
 
 function Pad(props){
+
+    let [isOn, setIsOn] = React.useState(false)
+
+    function handler(){
+        setIsOn(prev => !prev)
+    }
+
+
     return (
     <button 
+        onClick={handler}
         style={{backgroundColor: props.color}}
         className={`py-9 px-10 my-5 mx-8  rounded-md ${props.on ? 'opacity-100': 'opacity-30'}`}>
     </button>
