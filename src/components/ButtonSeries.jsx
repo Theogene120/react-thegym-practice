56b52bb79d7e01afb47a636dd4045d916a247606
Theogene <igiranezatheogene4@gmail.com>
2026-02-27 08:40:06 +0200

    changes

--- src/components/ButtonSeries.jsx
+++ src/components/ButtonSeries.jsx
@@ -5,27 +5,27 @@ function Pad(props){
 
     let [isOn, setIsOn] = React.useState(props.on)
 
-    function handler(){
-        setIsOn(prev => !prev)
-    }
+    
 
 
     return (
     <button 
-        onClick={handler}
+        onClick={() => props.toggle(props.id)}
         style={{backgroundColor: props.color}}
         className={`py-9 px-10 my-5 mx-8  rounded-md ${isOn ? 'opacity-100': 'opacity-30'}`}>
     </button>
 )
 }
 
-export default function ButtonSeries(props) {
+export default function ButtonSeries() {
 
-    
+    function handler(id){
+        console.log(id)
+    }
    
     let [button, setButton] = React.useState(pads)
     
-    let btn = button.map(b => <Pad key={b.id} color={b.color} on={b.on}/> )
+    let btn = button.map(b => <Pad toggle={handler} id={b.id}  key={b.id} color={b.color} on={b.on}/> )
     return (
         <main>
             <div className="pad-container">
