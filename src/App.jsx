d28b884fac97493b464df8596db6d64c5d287e6c
Theogene <igiranezatheogene4@gmail.com>
2026-02-27 16:59:45 +0200

    completion of chef claude project

--- src/App.jsx
+++ src/App.jsx
@@ -1,98 +1,59 @@
 
-import React from 'react'
-import ButtonSeries from './components/ButtonSeries'
-
+import trollFace from "/troll-face.png"
 function Header() {
-  return(
-    <div className="flex gap-1 justify-center items-center p-5 border rounded-t-md bg-white">
-      <img className="w-7" src="chef-claude-icon.png" alt="log" />
-      <p className="text-lg font-semibold">Chef Claude</p>
-    </div>
-  )
-}
+        return (
+            <header className=" flex items-center gap-3 pl-10 py-4 bg-gradient-to-r from-[#682281] to-[#A526D1]">
+                <img  className="h-6"
+                    src={trollFace} 
+                />
+                <h1 className="text-lg font-semibold text-white">Meme Generator</h1>
+            </header>
+        )
+    }
+
 
 function Main() {
-    const [ingredients, setIngredients] = React.useState([])
-    const [recipeShown, setRecipeShown] = React.useState(false)
-    
-    const list = ingredients.map(ingredient => (
-        <li key={ingredient}>{ingredient}</li>
-    ))
-
-    function handleSubmision(formData) {
-        console.log("Form submitted") // for checking
-        const newIngredients = formData.get('ingredient') // We pass form name as string
-
-        setIngredients(prevIngredients => [...prevIngredients, newIngredients])
-    }
-
-    function showRecipe(){
-        setRecipeShown(prev => !prev)
-    }
     return (
-        <>
-            <form action={handleSubmision} className="my-12 flex justify-center gap-5">
-                <input  className="border-2 border-gray-300 rounded-md w-80 outline-none pl-3" placeholder="e.g. oregano" type="text" name="ingredient" id="1" />
-                <button  className="bg-[#141413] text-white px-6 rounded-md h-8 text-sm font-semibold text-opacity-90">+ Add ingredient</button>
-            </form>
-            {ingredients.length == 0? '' : (<p className='font-bold text-2xl ml-10 pb-4'> Ingredients on hands: </p>)}
-            <ul className="list-disc ml-20">{list}</ul>
-            {ingredients.length <= 3? '' : (
-            <div className='flex justify-between items-center bg-gray-200 mt-10 m-6 py-4 px-8 rounded-lg'>
-                <div>
-                    <p className='font-bold'>Ready for a recipe?</p>
-                    <p className='text-sm font-semibold opacity-50'>Generate the recipe from your list of ingredient</p>
+        <main>
+            <div className="">
+                <div className="flex justify-evenly ">
+                    <label className="flex flex-col  w-full px-10  py-4 text-sm gap-1 font-semibold"><span>Top Text</span>
+                        <input
+                            className="font-normal rounded-md outline-none border-2 border-gray-300 pl-1 h-8 "
+                            type="text"
+                            placeholder="One does not simply"
+                            name="topText"
+                        />
+                    </label>
+
+                    <label className="flex flex-col w-full px-10  py-4 text-sm gap-1 font-semibold"><span>Bottom Text</span>
+                        <input
+                            className="font-normal rounded-md outline-none border-2 border-gray-300 pl-1 h-8 "
+                            type="text"
+                            placeholder="Walk into Mordor"
+                            name="bottomText"
+                        />
+                    </label>
                 </div>
-                <button onClick={showRecipe} className='bg-[#D17557] px-4 text-white font-bold text-sm rounded-lg h-9'>Get a recipe</button>
+                <button className=" mr-20 w-[555px] mx-10 mb-6 py-2 rounded-md text-white text-sm font-semibold bg-gradient-to-r from-[#682281] to-[#A526D1]">Get a new meme image 🖼</button>
             </div>
-        )}
-        {recipeShown && <div className='p-6'>
-            <h2 className='text-2xl font-bold mb-4'>Chef Claude Recommends:</h2>
-            <article className="suggested-recipe-container" aria-live="polite">
-            <p>Based on the ingredients you have available, I would recommend making a simple a delicious <strong>Beef Bolognese Pasta</strong>. Here is the recipe:</p>
-            <h3>Beef Bolognese Pasta</h3>
-            <p className='pt-4'><strong >Ingredients:</strong></p>
-            <ul className='list-disc ml-12 py-4'>
-                <li>1 lb. ground beef</li>
-                <li>1 onion, diced</li>
-                <li>3 cloves garlic, minced</li>
-                <li>2 tablespoons tomato paste</li>
-                <li>1 (28 oz) can crushed tomatoes</li>
-                <li>1 cup beef broth</li>
-                <li>1 teaspoon dried oregano</li>
-                <li>1 teaspoon dried basil</li>
-                <li>Salt and pepper to taste</li>
-                <li>8 oz pasta of your choice (e.g., spaghetti, penne, or linguine)</li>
-            </ul>
-            <p className='pt-4'><strong>Instructions:</strong></p>
-            <ol className='py-4 list-disc ml-12'>
-                <li>Bring a large pot of salted water to a boil for the pasta.</li>
-                <li>In a large skillet or Dutch oven, cook the ground beef over medium-high heat, breaking it up with a wooden spoon, until browned and cooked through, about 5-7 minutes.</li>
-                <li>Add the diced onion and minced garlic to the skillet and cook for 2-3 minutes, until the onion is translucent.</li>
-                <li>Stir in the tomato paste and cook for 1 minute.</li>
-                <li>Add the crushed tomatoes, beef broth, oregano, and basil. Season with salt and pepper to taste.</li>
-                <li>Reduce the heat to low and let the sauce simmer for 15-20 minutes, stirring occasionally, to allow the flavors to meld.</li>
-                <li>While the sauce is simmering, cook the pasta according to the package instructions. Drain the pasta and return it to the pot.</li>
-                <li>Add the Bolognese sauce to the cooked pasta and toss to combine.</li>
-                <li>Serve hot, garnished with additional fresh basil or grated Parmesan cheese if desired.</li>
-            </ol>
-            </article>
-        </div>}
-        </>
+            <div className="flex flex-col items-center">
+                <img className="rounded-lg" src="http://i.imgflip.com/1bij.jpg" />
+                <span className="-mt-80 text-4xl font-extrabold text-white">One does not simply</span>
+                <span className="mt-52 mb-20 text-4xl font-extrabold text-white">Walk into Mordor</span>
+            </div>
+        </main>
     )
 }
 
-function App() {
-    
+
+function App(){
     return (
         <>
-            {/* <Header />
-            <Main /> */}
-
-            <ButtonSeries darkMode = {false}/>
-
-        </>
-)
+            <Header />
+            <Main />
+        </> 
+    )
 }
 
 export default App
--- src/components/ButtonSeries.jsx
+++ src/components/ButtonSeries.jsx
@@ -5,25 +5,28 @@ function Pad(props){
 
     let [isOn, setIsOn] = React.useState(props.on)
 
-    
-
-
     return (
     <button 
         onClick={() => props.toggle(props.id)}
         style={{backgroundColor: props.color}}
-        className={`py-9 px-10 my-5 mx-8  rounded-md ${isOn ? 'opacity-100': 'opacity-30'}`}>
+        className={`py-9 px-10 my-5 mx-8  rounded-md ${props.on ? 'opacity-100': 'opacity-30'}`}>
     </button>
 )
 }
 
 export default function ButtonSeries() {
 
+    let [button, setButton] = React.useState(pads)
+
     function handler(id){
         console.log(id)
+        setButton(prev => prev.map(item => {
+            return item.id == id ? {...item, on: !item.on} : item
+        }))
+
     }
    
-    let [button, setButton] = React.useState(pads)
+    
     
     let btn = button.map(b => <Pad toggle={handler} id={b.id}  key={b.id} color={b.color} on={b.on}/> )
     return (
