44099adf55a166830d0089bd77916a20cf26cd99
Theogene <igiranezatheogene4@gmail.com>
2026-03-07 20:39:37 +0200

    todo project on going

--- src/Todo.jsx
+++ src/Todo.jsx
@@ -6,18 +6,29 @@ function Header() {
 
 function Form(){
     const [todo, setTodo] = React.useState([])
+    const [id, setId] = React.useState(1)
+    const [completeness, setCompleteness] = React.useState()
 
     function handleSubmit(formData){
         let work = formData.get('todo')
+        let newtodo = {id:id, text: work, completed: false}
         setTodo(prev => {
-            return [...prev, work]
+            return [...prev, newtodo]
         })
-        
+        setId(id + 1) // set id for next todo
     }
-    let todos = todo.map((value, idx) => (
-        <div key={idx}>
-            <input id={`todo-${idx}`} type="checkbox" name={value} value={value} />
-            <label htmlFor={`todo-${idx}`} >{value}</label>
+
+    function handleCheck(id){
+        setTodo(prev => prev.map(item => 
+            item.id === id ? {...item, completed: !item.completed} : item
+        )
+        )
+    }
+
+    let todos = todo.map(item => (
+        <div >
+            <input id={item.id} type="checkbox" onChange={() => handleCheck(item.id)} name={item.text} value={item.text} />
+            <label htmlFor={item.id}  className={`${item.completed ? "line-through text-gray-700" : " "}`} >{item.text}</label>
         </div>
     ))
 
@@ -25,7 +36,7 @@ function Form(){
         <div>
             <form action={handleSubmit} className="mt-10 flex border border-black rounded-full">
                 <input className="outline-none w-full rounded-full pl-4" type="text" name="todo" />
-                <button className=" m-1 mr-4 rounded-full bg-[#019090] text-white font-extrabold p-1"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
+                <button className="m-1 mr-4 rounded-full bg-[#019090] text-white font-extrabold p-1"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-plus-icon lucide-plus"><path d="M5 12h14"/><path d="M12 5v14"/></svg>
                 </button>
             </form>
             {todos}
