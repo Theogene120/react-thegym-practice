f57b104aad465be7fa92371d2f285562b48b811c
Theogene <igiranezatheogene4@gmail.com>
2026-03-08 16:47:50 +0200

    changes

--- src/Calculator.jsx
+++ src/Calculator.jsx
@@ -1,20 +1,46 @@
-
+import React from "react"
 
 function Button(){
     let buttons = ['AC','+/-','%','/','7','8','9','X','4','5','6','-','1','2','3','+','0','.','=']
     let sign = ['/','X','-','+','=']
 
-    let btns = buttons.map(btn => 
-        <div className={`${btn == '0'? 'grid col-span-2' : ''}`}>
-            <button className={`${sign.includes(btn) ? "bg-[#F38636] text-white text-3xl font-bold": "bg-gray-100 text-3xl font-bold"} w-full py-12 `}>
+    const [num, setNum] = React.useState(0)
+    const [s, setS] = React.useState([])
+
+    let btns = buttons.map((btn,idx) => 
+        <div key={idx} className={`${btn == '0'? 'grid col-span-2' : ''}`}>
+            <button onClick={() => handleClick(btn)} className={`${sign.includes(btn) ? "bg-[#F38636] text-white text-3xl font-bold": "bg-gray-100 text-3xl font-bold"} w-full py-12 `}>
                 {btn}
             </button>
         </div>
     )
+
+    function handleClick(value){
+
+        setNum(prev => {
+        if(isNaN(value)){
+            setS(pv => [...pv,value])
+            return prev
+        }else {
+            if(prev === 0){
+                setS(pv => [...pv,value])
+                return value
+            }else{
+                let newnum = `${prev}${value}`
+                setS(pv => [...pv,value])
+                return Number(newnum)
+            }   
+        }
+        })
+
+        console.log(s)
+        
+        
+    }
     
     return (
         <div>
-            <p className="h-20 text-6xl flex items-center justify-end text-white">0</p>
+            <p className="h-20 text-6xl flex items-center justify-end text-white">{num}</p>
             <div className="grid grid-cols-4 gap-[1px]">
                 {btns} 
             </div> 
