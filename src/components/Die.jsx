9ae690807485c59da763da8776984781e2fccf60
Theogene <igiranezatheogene4@gmail.com>
2026-03-13 14:37:57 +0200

    Add tenzie project

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,7 @@
 
 
 function Die(props){
-    return <button className="border px-8 py-2 font-bold text-2xl shadow-xl bg-gray-200">{props.value}</button>
+    return <button className="border  px-8 py-2 rounded-md font-bold text-2xl shadow-xl bg-gray-200">{props.value}</button>
 }
 
 export default Die
--- src/components/Tenzies.jsx
+++ src/components/Tenzies.jsx
@@ -1,22 +1,33 @@
+import React from "react"
+
 import Die from "./Die"
 
 function Main(){
+
+    const [nums, setNums] = React.useState([])
+
+    React.useEffect(() => {
+        let random = []
+        for(let i = 1; i <= 10; i++){
+            let n = Math.floor(Math.random() * 6) + 1
+            random.push(n)
+        }
+        
+        setNums(random)
+    }, [0])
+        
+        
+
+        let numbers = nums.map(num => <Die value={num} />)
+
+
     return (
         <div className="bg-[#0B2434] border rounded-lg h-[500px]">
             <div className="bg-white  h-[85%] rounded-md m-10">
                 <p>Discription</p>
                 <div className="flex justify-center mt-32">
                     <div className="grid grid-cols-5 gap-4">
-                        <Die value={1}/>
-                        <Die value={1}/>
-                        <Die value={1}/>
-                        <Die value={1}/>
-                        <Die value={1}/>
-                        <Die value={1}/>
-                        <Die value={1}/>
-                        <Die value={1}/>
-                        <Die value={1}/>
-                        <Die value={1}/>
+                        {numbers}
                     </div>
                 </div>
             </div>
