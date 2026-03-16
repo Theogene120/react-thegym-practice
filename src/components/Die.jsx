64451c0d814e4dcea2ab8612f16f8c009e653cba
Theogene <igiranezatheogene4@gmail.com>
2026-03-17 00:59:00 +0200

    Tenzies game on going

--- src/components/Die.jsx
+++ src/components/Die.jsx
@@ -1,7 +1,12 @@
 
 
 function Die(props){
-    return <button className="border  px-8 py-2 rounded-md font-bold text-2xl shadow-xl bg-gray-200">{props.value}</button>
+
+    return <button 
+    onClick={props.onClick} 
+    className={`border  px-8 py-2 rounded-md font-bold text-2xl shadow-xl  ${props.isHeld ? 'bg-[#59E391]' : 'bg-gray-200'}`}>
+        {props.value}
+    </button>
 }
 
 export default Die
--- src/components/Tenzies.jsx
+++ src/components/Tenzies.jsx
@@ -16,27 +16,35 @@ function Main(){
         return random
     }
 
+    function hold(id){
+        console.log(id)
+        setNums(prev => 
+            prev.map(die => die.id == id ? {...die, isHeld: !die.isHeld}: die)
+        )
+    }
+
     // General formula of generating random number in a give range()
 
     // Math.floor(Math.random() * (max - min + 1)) + min
 
-    let numbers = nums.map(numObj => <Die key={numObj.id} value={numObj.value} />)
+    let numbers = nums.map(numObj => <Die onClick={() => hold(numObj.id)} key={numObj.id} value={numObj.value} isHeld={numObj.isHeld} />)
 
-    function handleClick(){
-        setNums(randomNumbers())
+    function handleClick (){
+        setNums(prev => prev.map(die => die.isHeld ? die : {...die, value: Math.floor(Math.random() * 6) + 1}))
     }
 
 
     return (
-        <div className="bg-[#0B2434] border rounded-lg h-[500px]">
+        <div className="bg-[#0B2434] border rounded-lg h-[600px]">
             <div className="bg-white  h-[85%] rounded-md m-10">
-                <p>Discription</p>
-                <div className="flex justify-center mt-32">
+                <p className="text-center text-4xl pt-10 font-bold">Tenzies</p>
+                <p className="px-20 text-center pt-10 font-semibold opacity-80">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+                <div className="flex justify-center mt-24">
                     <div className="grid grid-cols-5 gap-4">
                         {numbers}
                     </div>
                 </div>
-                <div className="flex justify-center items-center mt-10"><button onClick={handleClick} className="bg-[#5035FF] text-white outline-none py-2 px-10 text-xl font-bold rounded-md shadow-2xl">Roll</button></div>
+                <div className="flex justify-center items-center mt-10"><button onClick={handleClick } className="bg-[#5035FF] text-white outline-none py-2 px-10 text-xl font-bold rounded-md shadow-2xl"> Roll </button></div>
             </div>
         </div>
     
