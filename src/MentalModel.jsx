2a7503410666a321c59e8b431c279abfc1dab4ca
Theogene <igiranezatheogene4@gmail.com>
2026-03-22 10:48:04 +0200

    changes on mental model

--- src/MentalModel.jsx
+++ src/MentalModel.jsx
@@ -4,8 +4,8 @@ function MentalModel(){
     const [text, setText] = React.useState('')
     const condition = [
         {label : 'At least 8 characters', isChecked: text.length >= 8},
-        {label : 'Contain uppercase character', isChecked: text.split('').some(v => v > 'A' && v < 'Z')},
-        {label : 'Contain lowercase character', isChecked: text.split('').some(v => v > 'a' && v < 'z')},
+        {label : 'Contain uppercase character', isChecked: text.split('').some(v => v >= 'A' && v <= 'Z')},
+        {label : 'Contain lowercase character', isChecked: text.split('').some(v => v >= 'a' && v <= 'z')},
         {label : 'Contain a digit', isChecked: text.split('').some(v => !isNaN(v))},
         {label : 'Contain a special character', isChecked: text.split('').some(ch => 
             !( (ch >= 'a' && ch <= 'z') ||
@@ -30,8 +30,8 @@ function MentalModel(){
 
     return(
         <div>
-            <div className="m-4 border border-black w-80 flex">
-                <input onChange={(e) => handleChanges(e)} className="outline-none pl-1 w-full" placeholder="Enter Password" type={show ? 'text' : 'password'} />
+            <div className="m-4 border border-black w-80 flex rounded-md">
+                <input onChange={(e) => handleChanges(e)} className="outline-none pl-1 w-full rounded-md" placeholder="Enter Strong Password" type={show ? 'text' : 'password'} />
                 <button onClick={handleShow}><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="lucide lucide-eye-icon lucide-eye"><path d="M2.062 12.348a1 1 0 0 1 0-.696 10.75 10.75 0 0 1 19.876 0 1 1 0 0 1 0 .696 10.75 10.75 0 0 1-19.876 0"/><circle cx="12" cy="12" r="3"/></svg></button>
             </div>
             <div>
