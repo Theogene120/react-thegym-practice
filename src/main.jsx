3860505cc308c43d8d110a30dff2f74defdbcdf7
Theogene <igiranezatheogene4@gmail.com>
2026-04-15 15:58:49 +0200

    changes

--- src/main.jsx
+++ src/main.jsx
@@ -16,20 +16,26 @@ import Login from "./MentalModel/QuestionOne";
 import Search from "./MentalModel/QuestionTwo";
 import Posts from "./MentalModel/QuestionThree";
 import Counter from "./MentalModel/QuestionFour";
-import GetData from "./MentalModel/QuestionFive";
+import GetData from "./MentalModel/GetData";
 import NameTransferLists from "./MentalModel/QuestionSeven";
 // import Counter from "./ReactGate/Question1";
 import ToggleButtons from "./ReactGate/Question3";
 import NameTransfer from "./ReactGate/Question4";
 import FetchData from "./ReactGate/FetchData";
 import CardList from "./ReactGate/Question5";
+import Context from "./Context";
 
 
 import ProductList from "./UKPractice/FirstQuestion";
+import UserProfileDemo from "./UKPractice/UserProfileDemo ";
+import TouchTypingApp from "./UKPractice/ThirdQuestion";
+
+import Routing from "./Routing";
+import FormSkills from "./FormSkills";
 
 import App from './components/Suspense'
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <GetData />
+    <FormSkills />
   </StrictMode>,
 );
